4c7f2d2f513602905a730f81d7845ddfbcaae5d2
William Burton <wdburton@umass.edu>
2021-12-06 23:37:38 -0500

    Added saving models and evaluation of saved modesl

--- data_processing/feature_analysis.py
+++ data_processing/feature_analysis.py
@@ -14,10 +14,21 @@ DEFAULT_PARAMS = {
     'norm': 'zscore'
 }
 
-
-def ranklib_evaluate(train: str, validate: str, test: str, params: dict = DEFAULT_PARAMS):
-    cmd = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -train {train} -validate {validate} -test {test} {" ".join([f"-{param} {value}" for param, value in params.items()])}'
+def ranklib_eval(train: str, validate: str, test: str, modelFile: str, params: dict = DEFAULT_PARAMS):
+    if(modelFile):
+        modelLoc = 'models/' + modelFile
+    cmd = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -load {modelFile} -train {train} -validate {validate} -test {test} -metric2T {params["metric2T"]}'
+    
     output = os.popen(cmd).read()
+
+    # If we where given a model to train, save the output of training here.
+    if(modelFile):
+        modelEvalLoc = 'models/EVAL_OUTPUT_' + modelFile
+        f = open(modelEvalLoc, "w")
+        f.write(output)
+        f.close()
+
+
     split_output = output.strip().split('\n')
 
     def extract_score(line: str) -> float:
@@ -29,6 +40,32 @@ def ranklib_evaluate(train: str, validate: str, test: str, params: dict = DEFAUL
         'test': extract_score(split_output[-1])
     }
 
+def ranklib_train(train: str, validate: str, test: str, modelFile: str, params: dict = DEFAULT_PARAMS):
+    if(modelFile):
+        modelLoc = 'models/' + modelFile
+    cmd = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -train {train} -validate {validate} -test {test} -save {modelLoc} {" ".join([f"-{param} {value}" for param, value in params.items()])}'
+    #os.system(cmd)
+    output = os.popen(cmd).read()
+
+    # If we where given a model to train, save the output of training here.
+    if(modelFile):
+        modelTrainLoc = 'models/TRAIN_OUTPUT_' + modelFile
+        f = open(modelTrainLoc, "w")
+        f.write(output)
+        f.close()
+
+    split_output = output.strip().split('\n')
+
+    def extract_score(line: str) -> float:
+        return float(line[line.index(':')+2:])
+
+    return {
+        'train': extract_score(split_output[-6]),
+        'validation': extract_score(split_output[-5]),
+        'test': extract_score(split_output[-3])
+    }
+
 
 if __name__ == '__main__':
-    print(ranklib_evaluate('ranklib/adjusted_train.txt', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt'))
+    print(ranklib_train('ranklib/adjusted_train.txt', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt', 'testFile.txt'))
+    print(ranklib_eval('ranklib/adjusted_train.txt', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt', 'testFile.txt'))
