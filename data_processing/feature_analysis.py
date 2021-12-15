074474cee118b95b125efbdd0c4cd9c1e34c7d99
William Burton <wdburton@umass.edu>
2021-12-15 07:07:11 -0500

    Got basic analysis done!

--- data_processing/feature_analysis.py
+++ data_processing/feature_analysis.py
@@ -23,6 +23,11 @@ DEFAULT_PARAMS = {
 # Variables for use in functions below
 FLDR_TRAIN = 'ranklib/'
 FLDR_OUTPUT = 'models/'
+FLDR_TRAIN_OUTPUT = FLDR_OUTPUT + 'trainingOutput/'
+FLD_FEATURE_EVAL = FLDR_OUTPUT + 'feature_evals/'
+FLD_FEATURE_PROC = FLDR_OUTPUT + 'feature_processed/'
+FLDR_ANALYZE_NORM = FLDR_OUTPUT + 'analyze/unadjusted/'
+FLDR_ANALYZE_ADJ = FLDR_OUTPUT + 'analyze/adjusted/'
 FLDR_KF = FLDR_OUTPUT + 'kft/'
 FLDR_SUMMARY = FLDR_OUTPUT + 'summary/'
 KFLD_SUFFIX = '.ca'
@@ -102,17 +107,118 @@ def ranklib_kfoldEvalProcess(modelFile: str = "TEST_NAME", kfoldNum: int = 1):
 
 # Evaluates the features on the model
 def ranklib_featureEval(modelFile: str):
-    modelLoc = modelFile#'models/' + modelFile + '.txt'
+    modelLoc = FLDR_OUTPUT + modelFile#'models/' + modelFile + '.txt'
     # NOTE -- the ';' in between MATH3_JAR and 'RANKLIB_JAR' is for windows systems only.  Try using ':' instead of it doesn't work.
     cmd = f'"{JAVA_HOME}/bin/java.exe"   -cp \"{MATH3_JAR}\";\"{RANKLIB_JAR}\" \"ciir.umass.edu.features.FeatureManager\" -feature_stats {modelLoc}'
     output = os.popen(cmd).read()
 
     # This is ABSOLUTELY ESSENTIAL -- not all features are always used!  This will let us know which ones weren't and which are
     # very important!
-    outputFile = modelFile + '_FeatureStatsRaw.txt'
+    outputFile = FLD_FEATURE_EVAL + modelFile + '_FeatureStatsRaw.txt'
     f = open(outputFile, "w")
     f.write(output)
     f.close()
+
+    return
+
+def ranklib_singFeatureProcess(modelFile: str):
+    # Preset variables used later on in the function 
+    # 
+    # Regex's used to extract the features used, and their frequences
+    featureNumRegex = '(?<=\[)\d+(?=\])'
+    featureFreqRegex = '(?<=\s\s)\d+(?=\n)'
+
+    # First the number of features
+    featureNames = pd.read_csv("ranklib/feature_info.csv")
+    numFeatures = len(featureNames)
+
+    # store feature id and value
+    featureList = [[0,0]] * numFeatures
+
+    # Now to set the list so that the feature number is correct for all of them!
+    for i in range(numFeatures):
+        featureList[i] = [i+1,0]
+
+
+    # First, we want to get the correct files out.
+    featureFile = FLD_FEATURE_EVAL + modelFile + '_FeatureStatsRaw.txt'
+    f = open(featureFile, "r")
+    rawFeatures = f.read()
+    f.close()
+
+    # Now we want to extract all of the correct features into a nice list of tuples.
+    # Extract them with regexes, and then...
+    featuresUsed = re.findall(featureNumRegex, rawFeatures)
+    featuresFreq = re.findall(featureFreqRegex, rawFeatures)
+    # ...After adjusting them to hold intigers instead of strings...
+    featuresUsed = list(map(int, featuresUsed))
+    featuresFreq = list(map(int, featuresFreq))
+    # ...and turn them into a list of tuples.
+    featuresTuples = list(zip(featuresUsed, featuresFreq))
+
+    # Now, we take those features tuples, and for each them, do some work with the features list.
+    for tuple in featuresTuples:
+        # Getting the actual feature value
+        curFeature = tuple[0] - 1
+
+        # Set feature value
+        featureList[curFeature][1] = tuple[1]
+
+    # Now that the stats are properly processed, time to order the array.
+    featureList.sort(key = lambda x: x[1], reverse = True)
+
+    # Now that we have that, method to create the string we want given the tuple:
+    def featureString(featureVals: list[int,int]) -> str:
+
+        # The list has the feature ID, the max, min, and then the average frequency.
+        # First, however, we need the feature name.
+        featureFrame = featureNames.loc[featureVals[0]-1].fillna("")
+
+        namePart_1 = featureFrame.loc['0']
+        namePart_2 = featureFrame.loc['1']
+        namePart_3 = featureFrame.loc['2']
+
+        # Now to slowly build up the string, using ljust for formatting
+        toReturnString = "Feature[" + (str(featureVals[0]) + "]:").rjust(5)
+        toReturnString = toReturnString.ljust(17)
+
+        # Should be starting at 15 characters
+        toReturnString += namePart_1
+        toReturnString = toReturnString.ljust(50)
+
+        # Should be starting at 45 characters
+        toReturnString += namePart_2
+        toReturnString = toReturnString.ljust(65)
+
+        toReturnString += namePart_3
+        toReturnString = toReturnString.ljust(80)
+        
+        toReturnString += " --  " + (str(featureVals[1])).rjust(3)
+        return toReturnString
+
+    finalList = ["ERROR"] * numFeatures
+
+    # First make the full list of features that we currently are using
+    for i in range(numFeatures):
+        finalList[i] = featureString(featureList[i])
+    
+    finDocName = FLD_FEATURE_PROC + modelFile + "_FeatureStatsProcessed.txt"
+    featuresFinalDoc = open(finDocName, "w")
+
+    # First off, the starting line of the summary document
+    startLine = ("ID".ljust(17) + "NAME").ljust(50)
+    startLine = (startLine + "TYPE").ljust(65)
+    startLine = (startLine + "PART").ljust(80)
+    startLine = startLine + " --  COUNT\n\n"
+    featuresFinalDoc.write(startLine)
+
+    transition = False
+    for i in range(numFeatures):
+        if( not transition and not featureList[i][1]):
+            featuresFinalDoc.write("\n----  NEVER USED ----\n\n")
+            transition = True
+        featuresFinalDoc.write(finalList[i])
+        featuresFinalDoc.write("\n")
 
     return
 
@@ -276,13 +382,13 @@ def ranklib_kfoldEval(kfoldNum, train: str, validate: str, test: str, params: di
 # Trains the models
 def ranklib_train(train: str, validate: str, test: str, modelFile: str, params: dict = DEFAULT_PARAMS):
     if(modelFile):
-        modelLoc = 'models/' + modelFile + '.txt'
+        modelLoc = FLDR_OUTPUT + modelFile
     cmd = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -train {train} -validate {validate} -test {test} -save {modelLoc} {" ".join([f"-{param} {value}" for param, value in params.items()])}'
     output = os.popen(cmd).read()
 
     # If we where given a location to save the model, save the normal console output at it's name '_TrainingOutput.txt'.
     if(modelFile):
-        modelTrainLoc = 'models/' + modelFile + '_TrainingOutput.txt'
+        modelTrainLoc = FLDR_TRAIN_OUTPUT + modelFile + '_TrainingOutput.txt'
         f = open(modelTrainLoc, "w")
         f.write(output)
         f.close()
@@ -292,11 +398,11 @@ def ranklib_train(train: str, validate: str, test: str, modelFile: str, params: 
     def extract_score(line: str) -> float:
         return float(line[line.index(':')+2:])
 
-    return {
+    """return {
         'train': extract_score(split_output[-6]),
         'validation': extract_score(split_output[-5]),
         'test': extract_score(split_output[-3])
-    }
+    }"""
 
 # Basic shuffling
 def ranklib_shuffle(train: str):
@@ -326,9 +432,117 @@ def ranklib_kfold_train(train: str, modelFile: str, params: dict = DEFAULT_PARAM
     print(split_output[-3])
 
 
+# WHAT WE NEED:
+# Train on a set of different training possibilities
+# See how those different sets of different training possibilities work with regards to a baseline
+
+def run_fin():
+
+    def getMetric(i: str):
+        if(i == "m"):
+            return "MAP"
+        if(i == "n"):
+            return "NDCG@10"
+        if(i == "d"):
+            return "DCG@10"
+        if(i == "p"):
+            return "P@10"
+        if(i == "r"):
+            return "RR@10"
+        if(i == "e"):
+            return "ERR@10"
+
+    def getName(i: str):
+        if(i == "m"):
+            return "MAP"
+        if(i == "n"):
+            return "NDCG"
+        if(i == "d"):
+            return "DCG"
+        if(i == "p"):
+            return "P"
+        if(i == "r"):
+            return "RR"
+        if(i == "e"):
+            return "ERR"
+
+    metrics = ["m","n","d","p","r","e"]
+    normalizeOptions = ["no_norm", "sum", "zscore", "linear"]
+
+    """for norm in normalizeOptions:
+        for train in metrics:
+            for test in metrics:
+
+                cur_params = {
+                    'ranker': 6,
+                    'metric2T': getMetric(test),
+                    'metric2t': getMetric(train),
+                }
+
+                if(norm != 'no_norm'):
+                    cur_params['norm'] = norm
+
+                modelName = norm + "_Tr-" + getName(train) + "_Te-" + getName(test)
+
+                # We now, for one loop, have the training and test metrics, as well as the model name.
+                # Now we need to train the models and get and store the feature evaluations.
+
+                ranklib_train('ranklib/adjusted_train.txt.shuffled', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt', modelName, cur_params)
+
+                # Now that the model is trained, it's time to get its feature evaluation
+                ranklib_featureEval(modelName)
+                ranklib_singFeatureProcess(modelName)
+
+                # Now we need to get the evaluation for adjusted and unadjusted
+                for i in metrics:
+                    output_1 = FLDR_ANALYZE_ADJ + i + '/' + modelName
+                    output_2 = FLDR_ANALYZE_NORM + i + '/' +  modelName
+                    modelLoc = FLDR_OUTPUT + modelName
+
+                    cmdAdj = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -load {modelLoc} -test ranklib/adjusted_test.txt -metric2T {getMetric(i)} -idv {output_1}'
+                    cmdNorm = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -load {modelLoc} -test ranklib/baseline_test.txt -metric2T {getMetric(i)} -idv {output_2}'
+                    os.popen(cmdAdj)
+                    os.popen(cmdNorm)
+                    pass
+
+                print( "Completed model: " + modelName )
+                
+    # We should now have all of the models trained, saved, evaluated, and with features extracted in both raw
+    # and processed formates.  Now, to make the baselines for the analysis.
+
+    for train in metrics:
+        outputAdj = FLDR_ANALYZE_ADJ + train + '/baseline'
+        outputNorm = FLDR_ANALYZE_NORM + train + '/baseline'
+        cmdA = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -test ranklib/adjusted_test.txt -metric2T {getMetric(train)} -idv {outputAdj}'
+        cmdB = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -test ranklib/baseline_test.txt -metric2T {getMetric(train)} -idv {outputNorm}'
+        os.popen(cmdA)
+        os.popen(cmdB)
+
+    print("Baseline Files Completed")
+
+    for i in metrics:
+        locA = FLDR_ANALYZE_ADJ + i + '/'
+        locB = FLDR_ANALYZE_NORM + i + '/'
+        nameA = FLDR_ANALYZE_ADJ + '/analysis/compilation_' + getName(i) + '.txt'
+        nameB = FLDR_ANALYZE_NORM + '/analysis/compilation_' + getName(i) + '.txt'
+        cmdA = f'"{JAVA_HOME}/bin/java.exe" -cp \"{RANKLIB_JAR}\" ciir.umass.edu.eval.Analyzer -all {locA} -base baseline > {nameA}'
+        cmdB = f'"{JAVA_HOME}/bin/java.exe" -cp \"{RANKLIB_JAR}\" ciir.umass.edu.eval.Analyzer -all {locB} -base baseline > {nameB}'
+        os.popen(cmdA)
+        os.popen(cmdB)
+        pass
+    """
+            
+    
+    
+    pass
+
+
+
+
 def run_all_test():
     print("Shuffling training data...")
-    ranklib_shuffle('ranklib/adjusted.txt')
+    ranklib_shuffle('ranklib/adjusted_train.txt')
+
     print("K-fold training...")
     #ranklib_kfold_train('ranklib/adjusted_train.txt.shuffled', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt', 'testFile')
     ranklib_kfold_train('ranklib/adjusted.txt.shuffled', 'testFile')
@@ -353,8 +567,17 @@ if __name__ == '__main__':
     #ranklib_kfold_train('ranklib/adjusted_train.txt', 'ranklib/adjusted_valid.txt', 'ranklib/adjusted_test.txt', 'testFile')
     #ranklib_featureEval('testFile')
     #ranklib_featureProcess('testFile', 5)
-    run_all_test()
+    #run_all_test()
     #ranklib_kfoldEvalProcess("test",5)
+
+
+    
+    #cmd = f'"{JAVA_HOME}/bin/java.exe" -jar \"{RANKLIB_JAR}\" -load models/kft/f1.ca -test ranklib/adjusted_test.txt -metric2T NDCG@10 -idv models/TEST2.txt'
+    #cmd = f'"{JAVA_HOME}/bin/java.exe" -cp \"{RANKLIB_JAR}\" ciir.umass.edu.eval.Analyzer -all models/analyze/ -base TEST.txt > testAnyl.txt'
+    #output = os.popen(cmd).read()
+    #print(output)
+
+    run_fin()
 
 # %%
 
