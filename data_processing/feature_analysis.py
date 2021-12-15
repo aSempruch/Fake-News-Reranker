51c51d30cecda5b837e14ae6317415db4520a853
William Burton <wdburton@umass.edu>
2021-12-15 08:24:41 -0500

    I DID IT, IT'S DONE, COLLECTED FEATURES WOOO!!

--- data_processing/feature_analysis.py
+++ data_processing/feature_analysis.py
@@ -26,11 +26,10 @@ FLDR_OUTPUT = 'models/'
 FLDR_TRAIN_OUTPUT = FLDR_OUTPUT + 'trainingOutput/'
 FLD_FEATURE_EVAL = FLDR_OUTPUT + 'feature_evals/'
 FLD_FEATURE_PROC = FLDR_OUTPUT + 'feature_processed/'
+FLDR_FEATURE_FIN = FLDR_OUTPUT + 'feature_fin/'
 FLDR_ANALYZE_NORM = FLDR_OUTPUT + 'analyze/unadjusted/'
 FLDR_ANALYZE_ADJ = FLDR_OUTPUT + 'analyze/adjusted/'
-FLDR_KF = FLDR_OUTPUT + 'kft/'
 FLDR_SUMMARY = FLDR_OUTPUT + 'summary/'
-KFLD_SUFFIX = '.ca'
 
 
 # TODO:  auto-training, auto-evaluation, print out and store extracted features with names
@@ -469,7 +468,7 @@ def run_fin():
     metrics = ["m","n","d","p","r","e"]
     normalizeOptions = ["no_norm", "sum", "zscore", "linear"]
 
-    """for norm in normalizeOptions:
+    for norm in normalizeOptions:
         for train in metrics:
             for test in metrics:
 
@@ -530,11 +529,190 @@ def run_fin():
         os.popen(cmdA)
         os.popen(cmdB)
         pass
-    """
+    
+    """       
+    # Now that all that is done with, it's time to process the variety of features in a more efficient manner.
+    # For this, we shall want features by:
+    #   Normalization value
+    #   Training value
+    #   Testing value
+    # Thus we shall start by making the folder lists that we will be wanting
+    # Then we shall send those lists into a function to make the output
+    # This should make a total of 16 files, all told.
+
+    featureFoldersTrain = []
+    featureFoldersTest = []
+    featureFoldersNorm = []
+
+    for i in metrics:
+        trainList = []
+        testList = []
+        for j in metrics:
+            for norm in normalizeOptions:
+                # As for the train list the train folders should be the same, and the reverse is true
+                # for the norm list, this is how we're doing it.
+                trainFld = FLD_FEATURE_PROC + norm + "_Tr-" + getName(i) + "_Te-" + getName(j) + "_FeatureStatsProcessed.txt"
+                testFld = FLD_FEATURE_PROC + norm + "_Tr-" + getName(j) + "_Te-" + getName(i) + "_FeatureStatsProcessed.txt"
+                trainList.append(trainFld)
+                testList.append(testFld)
+            pass
+        featureFoldersTrain.append(trainList)
+        featureFoldersTest.append(testList)
+    
+    for norm in normalizeOptions:
+        normList = []
+        for i in metrics:
+            for j in metrics:
+                fldr = FLD_FEATURE_PROC + norm + "_Tr-" + getName(i) + "_Te-" + getName(j) + "_FeatureStatsProcessed.txt"
+                normList.append(fldr)
+        featureFoldersNorm.append(normList)
+
+    # Now that we have the various lists, we need the processing functions.  Ho boy.
+
+    def procProccessedFeatures(fileList, fileName):
+        # Preset variables used later on in the function 
+
+        # First the number of features
+        featureNames = pd.read_csv("ranklib/feature_info.csv")
+        numFeatures = len(featureNames)
+
+        # Now, we need to store the values of the features -- max, min, and average -- in a list of the appropiate size.
+        featureList = [[0,0,0,0]] * numFeatures
+
+        # Now to set the list so that the feature number is correct for all of them!
+        for i in range(numFeatures):
+            featureList[i]=[i+1,0,0,0]
+
+        # Here we have a split based on if we have done any kfold training.  For now, we're going to assume we have.
+        # If we have, then we want to do a loop based on the number of kfold trainngs we have done.
+        for i in range(len(fileList)):
+            # First, we want to get the correct files out.
+            featureFile = fileList[i]
+            #print(featureFile)
+            f = open(featureFile, "r")
+            rawFeatures = f.read()
+            f.close()
+
+            # Now I gather all the numbers!
+            numbers = re.findall('(?<=\s)\d+', rawFeatures)
+
+            featuresTuples = [(0,0)] * numFeatures
+            for i in range(numFeatures):
+                curFeature = int(numbers[i*2])
+                curFreq = int(numbers[i*2+1])
+                featuresTuples[i] = (curFeature,curFreq)
+                if(curFeature > numFeatures):
+                    print("ERROR")
+                if(curFeature <= 0):
+                    print("ERROR")
+            #print(featuresTuples)
             
-    
-    
-    pass
+
+            # Now, we take those features tuples, and for each them, do some work with the features list.
+            for tuple in featuresTuples:
+                # Getting the actual feature value
+                curFeature = tuple[0] - 1
+
+                # Adding the tuple value to the total feature - used count.
+                featureList[curFeature][3] += tuple[1]
+
+                # Checking for maximum
+                if(tuple[1] > featureList[curFeature][1] ):
+                    featureList[curFeature][1] = tuple[1]
+
+                # Checking for minimum -- OR if the current minimum is 0
+                if((tuple[1] < featureList[curFeature][2]) or ( not featureList[curFeature][2])):
+                    featureList[curFeature][2] = tuple[1]
+        
+        
+        # Now that that's done, if we have done any kfold training, we need to adjust the values
+        for i in range(numFeatures):
+            curVal = featureList[i][3]
+            curVal = curVal/len(fileList)
+            curVal = round(curVal,2)
+            featureList[i][3] = curVal
+
+        # Now that the stats are properly processed, time to order the array.
+        featureList.sort(key = lambda x: x[3], reverse = True)
+
+        # Now that we have that, method to create the string we want given the tuple:
+        def featureString(featureVals: list[int,int,int,float]) -> str:
+
+            # The list has the feature ID, the max, min, and then the average frequency.
+            # First, however, we need the feature name.
+            featureFrame = featureNames.loc[featureVals[0]-1].fillna("")
+
+            namePart_1 = featureFrame.loc['0']
+            namePart_2 = featureFrame.loc['1']
+            namePart_3 = featureFrame.loc['2']
+
+            # Now to slowly build up the string, using ljust for formatting
+            toReturnString = "Feature[" + (str(featureVals[0]) + "]:").rjust(5)
+            toReturnString = toReturnString.ljust(17)
+
+            # Should be starting at 15 characters
+            toReturnString += namePart_1
+            toReturnString = toReturnString.ljust(50)
+
+            # Should be starting at 45 characters
+            toReturnString += namePart_2
+            toReturnString = toReturnString.ljust(65)
+
+            toReturnString += namePart_3
+            toReturnString = toReturnString.ljust(80)
+            
+            toReturnString += " -- " + (str(featureVals[1])).rjust(5)
+            toReturnString += " -- " + (str(featureVals[2])).rjust(5)
+            toReturnString += " -- " + (str(featureVals[3])).rjust(8)
+            return toReturnString
+
+        finalList = ["ERROR"] * numFeatures
+
+        # First make the full list of features that we currently are using
+        for i in range(numFeatures):
+            finalList[i] = featureString(featureList[i])
+        
+        finDocName = FLDR_FEATURE_FIN + fileName + ".txt"
+        featuresFinalDoc = open(finDocName, "w")
+
+        # First off, the starting line of the summary document
+        startLine = ("ID".ljust(17) + "NAME").ljust(50)
+        startLine = (startLine + "TYPE").ljust(65)
+        startLine = (startLine + "PART").ljust(80)
+        startLine = startLine + " --   MAX --   MIN --  AVERAGE\n\n"
+        featuresFinalDoc.write(startLine)
+
+        transition = False
+        for i in range(numFeatures):
+            if( not transition and not featureList[i][3]):
+                featuresFinalDoc.write("\n----  NEVER USED ----\n\n")
+                transition = True
+            featuresFinalDoc.write(finalList[i])
+            featuresFinalDoc.write("\n")
+
+        return
+
+    # And now, finally, we call upon this for each list of files!
+    # Done manually because EXHAUSTED
+    procProccessedFeatures(featureFoldersTest[0], "test_" + getName("m"))
+    procProccessedFeatures(featureFoldersTest[1], "test_" + getName("n"))
+    procProccessedFeatures(featureFoldersTest[2], "test_" + getName("d"))
+    procProccessedFeatures(featureFoldersTest[3], "test_" + getName("p"))
+    procProccessedFeatures(featureFoldersTest[4], "test_" + getName("r"))
+    procProccessedFeatures(featureFoldersTest[5], "test_" + getName("e"))
+
+
+    procProccessedFeatures(featureFoldersTrain[0], "train_" + getName("m"))
+    procProccessedFeatures(featureFoldersTrain[1], "train_" + getName("n"))
+    procProccessedFeatures(featureFoldersTrain[2], "train_" + getName("d"))
+    procProccessedFeatures(featureFoldersTrain[3], "train_" + getName("p"))
+    procProccessedFeatures(featureFoldersTrain[4], "train_" + getName("r"))
+    procProccessedFeatures(featureFoldersTrain[5], "train_" + getName("e"))
+
+    procProccessedFeatures(featureFoldersNorm[0], "norm_none")
+    procProccessedFeatures(featureFoldersNorm[1], "norm_sum")
+    procProccessedFeatures(featureFoldersNorm[2], "norm_zscore")
+    procProccessedFeatures(featureFoldersNorm[3], "norm_linear")"""
 
 
 
